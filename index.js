1693266bbea35b5eb53e35222fd35b0affa23588
mercy-tarus <merciechelah1@gmail.com>
2024-04-02 23:07:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+ const drivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers)
+{
+
+  return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers)
+{
+  
+  return drivers.slice(-2);
+
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+}
+}
+function fareDoubler(){
+  return 10*2;
+}
+function fareTripler(){
+  let x=12;
+  return x*3;////36
+}
+function selectDifferentDrivers(drivers, func)
+{
+  return func(drivers);
+}
